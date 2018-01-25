74597e4457c17261c9ac65e66081a4430f0b7811
bhavnapatil96 <patilbhavna96@gmail.com>
2018-01-25 18:22:20 +0530

    Moongoos Added

--- models/user.js
+++ models/user.js
@@ -1,11 +1,46 @@
 var mongoose=require('mongoose');
+const jwt=require('jsonwebtoken');
 
-var user=mongoose.model('user',{
+var UserSchema=new mongoose.Schema({
     email:{
         required:true,
         type:String,
         minlength:1,
-        trim:true
-    }
-});
-module.exports={user};
+        trim:true,
+        unique:true,
+        // validate:{
+        //     validator:validator.isEmail,
+        //     message:'{value} not a email'
+        // }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens:[{
+        access:{
+            type:String,
+
+        },
+        token:{
+            type:String,
+
+        }
+    }]
+})
+
+UserSchema.methods.generateAuthToken=function(){
+   // console.log(`gen`);
+    var user=this;
+   // console.log(this);
+    var access='auth';
+    var token=jwt.sign({_id: user._id.toHexString(),access},'abc123').toString();
+
+    user.tokens.push({access,token});
+    return user.save().then(()=>{
+       return token;
+    });
+};
+var User=mongoose.model('user',UserSchema);
+module.exports={User};
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express=require('express');
 var bodyParser=require('body-parser');
+var _=require('lodash');
 var {ObjectID}=require('mongodb');
 var {todo}=require('./models/todo');
-var {user}=require('./models/user');
+var {User}=require('./models/User');
 let mongoose1=require('./db/mongoose1');
 var app=express();
 app.use(bodyParser.json());
@@ -19,6 +20,37 @@ app.post('/todos',(req,res)=>{
      },(e)=>{
          res.status(400).send(e);
      });
+});
+
+app.post('/users',(req,res)=>{
+    console.log(`users`);
+    // var newuser=new User({
+    //     email:req.body.email,
+    //     password:req.body.password
+    // });
+
+    var body=_.pick(req.body,['email','password']);
+    console.log(body);
+    var user=new User(body);
+
+    // user.save().then((data)=>{
+    //     console.log(`saved`);
+    //     let token = user.generateAuthToken();
+    //     token.then((t)=>{
+    //         res.header('x-auth',t).send(user);
+    //     }).catch((err)=>{console.log(err.message)});
+    // }).catch((err)=>{console.log(err.message)});
+
+    user.save().then((data)=>{
+        console.log(`inside save`);
+        let token = user.generateAuthToken();
+
+
+    }).then((token)=>{
+        res.header('x-auth',token).send(user);
+    }).catch((e)=>{
+        res.status(400).send();
+    });
 });
 
 app.get('/todos',(req,res)=>{
@@ -47,34 +79,57 @@ app.get('/todos/:id',(req,res)=>{
         res.status(404).send();
     });
 });
-//
-// newtodo.save().then((doc)=>{
-//     console.log(doc);
-//
-// },(e)=>{
-//     console.log('unable to save',e);
-// });
-// var othertodo=new todo({
-//      text:'eat',
-//     completed:true,
-//      completedAt:123
-// });
- // othertodo.save().then((doc)=>{
- //     console.log(JSON.stringify(doc,undefined,2));
- // },(e)=>{
- //     console.log('unable to save.....');
- // });
 
-// var newUser=new user({
-//     email:'patilbhavna96@gmail.com'
-// });
-//
-// newUser.save().then((doc)=>{
-//     console.log(JSON.stringify(doc));
-//
-// },(e)=>{
-//     console.log('unable to save',e);
-// });
+app.delete('/todos/:id',(req,res)=>{
+    var id=req.params.id;
+    if(!ObjectID.isValid(id))
+    {
+        return res.status(404).send();
+    }
+
+    todo.findByIdAndRemove(id).then((todo)=>{
+        if(!todo)
+        {
+            res.status(404).send();
+        }
+        res.send({todo});
+    },(e)=>{
+        res.status(404).send();
+    }).catch((e)=>{
+        res.status(404).send();
+    });
+});
+app.patch('/todos/:id',(req,res)=>{
+    var id=req.params.id;
+
+    var body=_.pick(req.body,['text','completed']);
+    if(!ObjectID.isValid(id))
+    {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed)&& body.completed)
+    {
+        body.completed=new Date().getTime();
+    }
+    else {
+        body.completed=false;
+        body.completedAt=null;
+    }
+
+    todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+        if(!todo)
+        {
+            res.status(404).send();
+        }
+        res.send({todo});
+    },(e)=>{
+        res.status(404).send();
+    }).catch((e)=>{
+        res.status(404).send();
+    });
+});
+
 
 
 app.listen(8081,()=>{
